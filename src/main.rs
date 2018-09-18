1add7024fac6920754905d3d297cc7f6f103b2f7
Ben-PH <benphawke@gmail.com>
2018-09-18 18:42:42 +1000

    Make free-standing binary.
    After getting rid of the std library, need to get rid
    of main, make sure we have a panic implementation and
    give an entry point using C convention.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#![feature(panic_implementation)]
 #![no_std]
+#![no_main]
 
-fn main() {}
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    // Entry point, We need to give the linker something to work with.
+    loop {}
+}
+
+
+#[panic_handler]
+#[no_mangle]
+pub fn panic(_info: &PanicInfo) -> ! {
+    loop{}
+}
+
