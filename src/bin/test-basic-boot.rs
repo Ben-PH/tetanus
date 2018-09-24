3e08a4319b1987922dbd792c6765a5d28f9bd926
Ben-PH <benphawke@gmail.com>
2018-09-24 15:27:19 +1000

    Add basic boot test that I forgot to commit.

---
+++ src/bin/test-basic-boot.rs
@@ -0,0 +1,31 @@
+#![feature(panic_handler)]
+#![no_std]
+#![cfg_attr(not(test), no_main)]
+#![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
+
+#[macro_use]
+extern crate blog_os;
+
+use core::panic::PanicInfo;
+use blog_os::exit_qemu;
+
+#[cfg(not(test))]
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    panic!("PANIC! at the disco");
+    serial_println!("should not get here :/");
+    unsafe {exit_qemu();}
+    loop {}
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+#[no_mangle]
+pub fn panic(info: &PanicInfo) -> ! {
+    serial_println!("failed");
+
+    serial_println!("{}", info);
+
+    unsafe { exit_qemu(); }
+    loop {}
+}
